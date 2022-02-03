49a42ac5d4c358fa8a3dd81f063520baa6c59b9b
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-02-03 20:26:31 -0300

    adiciona até o exercicio 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia2-javascript-array-e-loop-for/exercicio2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia2-javascript-array-e-loop-for/exercicio2.js
@@ -1,5 +1,61 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let i = 0; i< numbers.length; i += 1){
+/* for (let i = 0; i < numbers.length; i += 1){
     console.log(numbers[i]);
+} */
+
+/* let somaNumeros = 0;
+for (let i = 0; i < numbers.length; i += 1){
+    somaNumeros = somaNumeros + numbers[i];
 }
+
+console.log(somaNumeros); */
+
+/* let somaNumeros = 0;
+
+for (let i = 0; i < numbers.length; i += 1){
+    somaNumeros = somaNumeros + numbers[i];
+}
+
+let media = somaNumeros / numbers.length;
+ console.log(media);
+
+if (media > 20){
+    console.log("valor maior que 20");
+}
+else{
+    console.log("valor menor ou igual a 20");
+} */
+
+/* let maiorNumero = 0;
+
+for (let i = 0; i < numbers.length; i += 1){
+    if (numbers[i] > maiorNumero){
+        maiorNumero = numbers[i]
+    }
+}
+
+console.log(maiorNumero); */
+
+/* let valorImpar = 0;
+
+for (let i = 0; i < numbers.length; i += 1){
+    if (numbers[i] % 2 !== 0){
+    valorImpar += 1;
+    }
+}
+if (valorImpar == 0){
+    console.log("nenhum valor ímpar encontrado");
+} else{
+    console.log(valorImpar);
+} */
+
+let menorValor = numbers[0];
+
+for (let i = 0; i < numbers.length; i += 1){
+    if (numbers[i] < menorValor){
+        menorValor = numbers[i];
+    }
+}
+
+console.log(menorValor);
