7b018f2c817167e014d7da2f565327b451d6e170
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-02-09 18:23:17 -0300

    adiciona o diretorio do novo bloco

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,11 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+
+document.getElementById("page-title").innerText = 'Toy Story'; 
+document.getElementById("second-paragraph").innerText = 'Filme de animação clássico';
+document.getElementById("subtitle").innerText = 'A Revanche';
+
+
+//document.getElementsByClassName("paragrafo")[0].innerText = 'Parágrafo alterado'
+const subtitle = document.getElementsByTagName("h4")[0];
+    subtitle.style.color = 'blue';
