f743e9a4d2951e9283f49cf8c534d89b490fe41c
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-04-05 10:40:20 -0300

    faz alterações no exercício hello world

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/hello-world/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/hello-world/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const Task = (value, index) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li className="task" key={index} >{value}</li>
   );
+}
+
+const compromissos = ['Meditar', 'Fazer exercício', 'Estudar', 'Se alimentar'];
+
+// const teste = compromissos.map(compromisso => Task(compromisso));
+// console.log(teste);
+
+class App extends React.Component {
+  render(){
+    return (
+      <ul>{ compromissos.map(compromisso => Task(compromisso)) }</ul> 
+    );
+  }
 }
 
 export default App;
