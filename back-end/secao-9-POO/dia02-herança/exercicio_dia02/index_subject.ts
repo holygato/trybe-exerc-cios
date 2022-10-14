4ccff33ca9d3d0cd577aabb8901ef4138d59454a
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-10-14 09:24:49 -0300

    Sobe as alterações da seção 9.02

---
+++ back-end/secao-9-POO/dia02-herança/exercicio_dia02/index_subject.ts
@@ -0,0 +1,12 @@
+import Subject from './Subject';
+
+const math = new Subject('Matemática');
+const story = new Subject('História');
+const philosophy = new Subject('Filosofia');
+
+console.log(math);
+console.log(story);
+console.log(philosophy);
+
+// deve retornar erro
+// const invalidSubjectName = new Subject('Po');
